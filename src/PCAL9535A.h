12f31b0274bcda5ba85b268d3167747a32fd8ecf
Chris Barr <chris@chrisbarrbuilds.com>
2022-03-05 11:49:38 +1030

    Replace #defines with constexpr

--- src/PCAL9535A.h
+++ src/PCAL9535A.h
@@ -11,51 +11,51 @@
 
 namespace PCAL9535A {
 
-#define PCAL9535A_ADDRESS 0x20
+constexpr int PCAL9535A_ADDRESS = 0x20;
 
 // registers
-#define PCAL9535A_P0_INPUT    0x00
-#define PCAL9535A_P0_OUTPUT   0x02
-#define PCAL9535A_P0_POLINV   0x04
-#define PCAL9535A_P0_CONFIG   0x06
-#define PCAL9535A_P0_DRVSTR1  0x40
-#define PCAL9535A_P0_DRVSTR2  0x41
-#define PCAL9535A_P0_ILATCH   0x44
-#define PCAL9535A_P0_PULLENA  0x46
-#define PCAL9535A_P0_PULLSEL  0x48
-#define PCAL9535A_P0_INTMASK  0x4A
-#define PCAL9535A_P0_INTSTAT  0x4C
+constexpr int PCAL9535A_P0_INPUT    = 0x00;
+constexpr int PCAL9535A_P0_OUTPUT   = 0x02;
+constexpr int PCAL9535A_P0_POLINV   = 0x04;
+constexpr int PCAL9535A_P0_CONFIG   = 0x06;
+constexpr int PCAL9535A_P0_DRVSTR1  = 0x40;
+constexpr int PCAL9535A_P0_DRVSTR2  = 0x41;
+constexpr int PCAL9535A_P0_ILATCH   = 0x44;
+constexpr int PCAL9535A_P0_PULLENA  = 0x46;
+constexpr int PCAL9535A_P0_PULLSEL  = 0x48;
+constexpr int PCAL9535A_P0_INTMASK  = 0x4A;
+constexpr int PCAL9535A_P0_INTSTAT  = 0x4C;
 
-#define PCAL9535A_P1_INPUT    0x01
-#define PCAL9535A_P1_OUTPUT   0x03
-#define PCAL9535A_P1_POLINV   0x05
-#define PCAL9535A_P1_CONFIG   0x07
-#define PCAL9535A_P1_DRVSTR1  0x42
-#define PCAL9535A_P1_DRVSTR2  0x43
-#define PCAL9535A_P1_ILATCH   0x45
-#define PCAL9535A_P1_PULLENA  0x47
-#define PCAL9535A_P1_PULLSEL  0x49
-#define PCAL9535A_P1_INTMASK  0x4B
-#define PCAL9535A_P1_INTSTAT  0x4D
+constexpr int PCAL9535A_P1_INPUT    = 0x01;
+constexpr int PCAL9535A_P1_OUTPUT   = 0x03;
+constexpr int PCAL9535A_P1_POLINV   = 0x05;
+constexpr int PCAL9535A_P1_CONFIG   = 0x07;
+constexpr int PCAL9535A_P1_DRVSTR1  = 0x42;
+constexpr int PCAL9535A_P1_DRVSTR2  = 0x43;
+constexpr int PCAL9535A_P1_ILATCH   = 0x45;
+constexpr int PCAL9535A_P1_PULLENA  = 0x47;
+constexpr int PCAL9535A_P1_PULLSEL  = 0x49;
+constexpr int PCAL9535A_P1_INTMASK  = 0x4B;
+constexpr int PCAL9535A_P1_INTSTAT  = 0x4D;
 
-#define PCAL9535A_OUTPUT_CONF 0x4F
+constexpr int PCAL9535A_OUTPUT_CONF = 0x4F;
 
-#define PCAL9535A_DRVSTR_25   0x00
-#define PCAL9535A_DRVSTR_50   0x01
-#define PCAL9535A_DRVSTR_75   0x10
-#define PCAL9535A_DRVSTR_100  0x11
-#define PCAL9535A_PULLENA_DISABLED  0x00
-#define PCAL9535A_PULLENA_ENABLED   0x01
-#define PCAL9535A_PULLSEL_PULLDOWN  0x00
-#define PCAL9535A_PULLSEL_PULLUP    0x01
-#define PCAL9535A_OUTPUT_CONF_PP    0x00
-#define PCAL9535A_OUTPUT_CONF_OD    0x01
+constexpr int PCAL9535A_DRVSTR_25   = 0x00;
+constexpr int PCAL9535A_DRVSTR_50   = 0x01;
+constexpr int PCAL9535A_DRVSTR_75   = 0x10;
+constexpr int PCAL9535A_DRVSTR_100  = 0x11;
+constexpr int PCAL9535A_PULLENA_DISABLED  = 0x00;
+constexpr int PCAL9535A_PULLENA_ENABLED   = 0x01;
+constexpr int PCAL9535A_PULLSEL_PULLDOWN  = 0x00;
+constexpr int PCAL9535A_PULLSEL_PULLUP    = 0x01;
+constexpr int PCAL9535A_OUTPUT_CONF_PP    = 0x00;
+constexpr int PCAL9535A_OUTPUT_CONF_OD    = 0x01;
 
-#define PULL_NONE 0
-#define PULL_UP   1
-#define PULL_DOWN 2
+constexpr int PULL_NONE = 0;
+constexpr int PULL_UP   = 1;
+constexpr int PULL_DOWN = 2;
 
-#define PCAL9535A_INT_ERR 255
+constexpr int PCAL9535A_INT_ERR = 255;
 
 class PCAL9535A {
 public:
