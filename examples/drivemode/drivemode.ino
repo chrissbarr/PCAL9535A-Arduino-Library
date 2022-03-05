b8f8f1b3d8d6646d336249aa4208ff78c228acea
Chris Barr <chris@chrisbarrbuilds.com>
2022-03-05 23:22:34 +1030

    Add drivemode example

---
+++ examples/drivemode/drivemode.ino
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include "PCAL9535A.h"
+
+PCAL9535A::PCAL9535A gpio;
+
+constexpr int port0testpin = 0;
+constexpr int port1testpin = 8;
+  
+void setup() {  
+  gpio.begin();
+  gpio.portSetOutputMode(0, PCAL9535A::DriveMode::OPENDRAIN);
+  gpio.portSetOutputMode(1, PCAL9535A::DriveMode::PUSHPULL);
+  gpio.pinMode(port0testpin, OUTPUT);
+  gpio.pinMode(port1testpin, OUTPUT);
+}
+
+void loop() {
+  gpio.digitalWrite(port0testpin, HIGH);
+  gpio.digitalWrite(port1testpin, HIGH);
+  delay(100);
+  gpio.digitalWrite(port0testpin, LOW);
+  gpio.digitalWrite(port1testpin, LOW);
+  delay(100);
+}
