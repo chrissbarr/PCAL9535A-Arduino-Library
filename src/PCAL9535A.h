e0e9d23c6324c318217b38c405ed8cf2aca9e421
Chris Barr <chris@chrisbarrbuilds.com>
2022-03-06 14:19:36 +1030

    WIP change to template

--- src/PCAL9535A.h
+++ src/PCAL9535A.h
@@ -8,6 +8,7 @@
 #define _PCAL9535A_H_
 
 #include <stdint.h>
+#include <Arduino.h>
 
 namespace PCAL9535A {
 
@@ -86,8 +87,11 @@ enum class Port : uint8_t {
 
 constexpr int PCAL9535A_INT_ERR = 255;
 
+template <typename WIRE>
 class PCAL9535A {
 public:
+
+  explicit PCAL9535A(WIRE& wire) : mWire(wire) {}
   /**
    * Initializes the PCAL9535A given its HW address, see datasheet for address selection.
    * /param addr Address of PCAL9535A (0 - 7)
@@ -202,6 +206,7 @@ public:
 
  private:
   uint8_t _i2caddr;
+  WIRE& mWire;
 
   /**
    * Convert a given pin (0 - 15) to a port bit number (0 - 7)
@@ -245,6 +250,211 @@ public:
   void updateRegisterBit(uint8_t pin, uint8_t value, RegisterAddress port0, RegisterAddress port1);
 
 };
+
+template <typename WIRE>
+void PCAL9535A<WIRE>::begin(HardwareAddress addr) {
+
+	_i2caddr = PCAL9535A_ADDRESS | static_cast<uint8_t>(addr);
+
+	mWire.begin();
+
+	// set all pins as inputs
+	writeRegister(RegisterAddress::P0_CONFIG, 0xff);
+	writeRegister(RegisterAddress::P1_CONFIG, 0xff);
+}
+
+template <typename WIRE>
+void PCAL9535A<WIRE>::begin() {
+	begin(HardwareAddress::A000);
+}
+
+template <typename WIRE>
+void PCAL9535A<WIRE>::pinMode(uint8_t pin, uint8_t mode) {
+	updateRegisterBit(pin, (mode == INPUT ? 1 : 0), RegisterAddress::P0_CONFIG, RegisterAddress::P1_CONFIG);
+}
+
+template <typename WIRE>
+uint8_t PCAL9535A<WIRE>::readGPIO(Port port) {
+	mWire.beginTransmission(_i2caddr);
+	mWire.write(static_cast<uint8_t>(port == Port::P0 ? RegisterAddress::P0_INPUT : RegisterAddress::P1_INPUT));
+	mWire.endTransmission();
+	mWire.requestFrom(_i2caddr, uint8_t(1));
+	return mWire.read();
+}
+
+template <typename WIRE>
+uint16_t PCAL9535A<WIRE>::readGPIO16() {
+	uint16_t val = 0;
+	uint8_t p0;
+
+	// read the current GPIO inputs
+	mWire.beginTransmission(_i2caddr);
+	mWire.write(static_cast<uint8_t>(RegisterAddress::P0_INPUT));
+	mWire.endTransmission();
+
+	mWire.requestFrom(_i2caddr, uint8_t(2));
+	p0 = mWire.read();
+	val = mWire.read();
+	val <<= 8;
+	val |= p0;
+
+	return val;
+}
+
+template <typename WIRE>
+void PCAL9535A<WIRE>::writeGPIO(Port port, uint8_t val) {
+	mWire.beginTransmission(_i2caddr);
+	mWire.write(static_cast<uint8_t>(port == Port::P0 ? RegisterAddress::P0_OUTPUT : RegisterAddress::P1_OUTPUT));
+	mWire.write(val);
+	mWire.endTransmission();
+}
+
+template <typename WIRE>
+void PCAL9535A<WIRE>::writeGPIO16(uint16_t val) {
+	mWire.beginTransmission(_i2caddr);
+	mWire.write(static_cast<uint8_t>(RegisterAddress::P0_OUTPUT));
+	mWire.write(val & 0xFF);
+	mWire.write(val >> 8);
+	mWire.endTransmission();
+}
+
+template <typename WIRE>
+void PCAL9535A<WIRE>::digitalWrite(uint8_t pin, uint8_t val) {
+	const RegisterAddress regAddr = pinToReg(pin, RegisterAddress::P0_OUTPUT, RegisterAddress::P1_OUTPUT);
+
+	// read the current GPIO state, so we can modify only this one pin
+	uint8_t gpio = readRegister(regAddr);
+
+	// update the GPIO state with the pin we wish to set
+	bitWrite(gpio, pinToBit(pin), val);
+
+	// write the new GPIO state back out
+	writeRegister(regAddr, gpio);
+}
+
+template <typename WIRE>
+uint8_t PCAL9535A<WIRE>::digitalRead(uint8_t pin) {
+	const RegisterAddress regAddr = pinToReg(pin, RegisterAddress::P0_INPUT, RegisterAddress::P1_INPUT);
+	return (readRegister(regAddr) >> pinToBit(pin)) & 0x1;
+}
+
+template <typename WIRE>
+void PCAL9535A<WIRE>::pinSetPull(uint8_t pin, PullSetting pull) {
+	updateRegisterBit(pin, static_cast<uint8_t>((pull == PullSetting::NONE) ? RegisterValues_PULLENA::DISABLED : RegisterValues_PULLENA::ENABLED), RegisterAddress::P0_PULLENA, RegisterAddress::P1_PULLENA);
+	updateRegisterBit(pin, static_cast<uint8_t>((pull == PullSetting::UP) ? RegisterValues_PULLSEL::PULLUP : RegisterValues_PULLSEL::PULLDOWN), RegisterAddress::P0_PULLSEL, RegisterAddress::P1_PULLSEL);	
+}
+
+template <typename WIRE>
+void PCAL9535A<WIRE>::pinSetDriveStrength(uint8_t pin, DriveStrength strength) {
+	RegisterAddress regAddr;
+	uint8_t regValue;
+
+	if (pinToBit(pin) < 4) {
+		regAddr = pinToReg(pin, RegisterAddress::P0_DRVSTR1, RegisterAddress::P1_DRVSTR1);
+	}
+	else
+	{
+		regAddr = pinToReg(pin, RegisterAddress::P0_DRVSTR2, RegisterAddress::P1_DRVSTR2);
+	}
+
+ 	regValue = readRegister(regAddr);
+ 	regValue |= (static_cast<uint8_t>(strength) & 0x03) << ((pin % 4) * 2);
+
+	writeRegister(regAddr, regValue);
+}
+
+template <typename WIRE>
+void PCAL9535A<WIRE>::pinSetInputInversion(uint8_t pin, bool invert) {
+	updateRegisterBit(pin, (invert ? 1 : 0), RegisterAddress::P0_POLINV, RegisterAddress::P1_POLINV);
+}
+
+template <typename WIRE>
+void PCAL9535A<WIRE>::pinSetInputLatch(uint8_t pin, bool latch) {
+	updateRegisterBit(pin, (latch ? 1 : 0), RegisterAddress::P0_ILATCH, RegisterAddress::P1_ILATCH);
+}
+
+template <typename WIRE>
+void PCAL9535A<WIRE>::pinSetInterruptEnabled(uint8_t pin, bool enabled) {
+	updateRegisterBit(pin, (enabled ? 1 : 0), RegisterAddress::P0_INTMASK, RegisterAddress::P1_INTMASK);
+}
+
+template <typename WIRE>
+uint8_t PCAL9535A<WIRE>::getLastInterruptPin() {
+	uint8_t intf;
+
+	intf = readRegister(RegisterAddress::P0_INTSTAT);
+	for(int i = 0; i < 8; i++) {
+		if (bitRead(intf, i)) {
+			return i;
+		}
+	}
+
+	intf = readRegister(RegisterAddress::P1_INTSTAT);
+	for(int i = 0; i < 8; i++) {
+		if (bitRead(intf, i)) {
+			return i + 8;
+		}
+	}
+
+	return PCAL9535A_INT_ERR;
+}
+
+template <typename WIRE>
+uint8_t PCAL9535A<WIRE>::getInterruptPinValue() {
+	uint8_t intPin = getLastInterruptPin();
+
+	if(intPin != PCAL9535A_INT_ERR){
+		return (digitalRead(intPin));
+	}
+
+	return PCAL9535A_INT_ERR;
+}
+
+template <typename WIRE>
+void PCAL9535A<WIRE>::portSetOutputMode(Port port, DriveMode mode) {
+	uint8_t regValue = readRegister(RegisterAddress::OUTPUT_CONF);
+	bitWrite(regValue, (port == Port::P0 ? 0 : 1), (static_cast<uint8_t>(mode) & 0x01));
+	writeRegister(RegisterAddress::OUTPUT_CONF, regValue);
+}
+
+template <typename WIRE>
+uint8_t PCAL9535A<WIRE>::pinToBit(uint8_t pin) const {
+	return pin % 8;
+}
+
+template <typename WIRE>
+RegisterAddress PCAL9535A<WIRE>::pinToReg(uint8_t pin, RegisterAddress port0, RegisterAddress port1) const {
+	return (pin < 8) ? port0 : port1;
+}
+
+template <typename WIRE>
+uint8_t PCAL9535A<WIRE>::readRegister(RegisterAddress reg) {
+	// read the current GPINTEN
+	mWire.beginTransmission(_i2caddr);
+	mWire.write(static_cast<uint8_t>(reg));
+	mWire.endTransmission();
+	mWire.requestFrom(_i2caddr, uint8_t(1));
+	return mWire.read();
+}
+
+template <typename WIRE>
+void PCAL9535A<WIRE>::writeRegister(RegisterAddress reg, uint8_t regValue) {
+	// Write the register
+	mWire.beginTransmission(_i2caddr);
+	mWire.write(static_cast<uint8_t>(reg));
+	mWire.write(regValue);
+	mWire.endTransmission();
+}
+
+template <typename WIRE>
+void PCAL9535A<WIRE>::updateRegisterBit(uint8_t pin, uint8_t pValue, RegisterAddress port0, RegisterAddress port1) {
+	const RegisterAddress regAddr = pinToReg(pin, port0, port1);
+	uint8_t regValue = readRegister(regAddr);
+
+	// set the value for the particular bit
+	bitWrite(regValue, pinToBit(pin), pValue);
+	writeRegister(regAddr, regValue);
+}
 
 } // namespace PCAL9535A
 
